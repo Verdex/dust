71e40d0986d576b599ae7358ca23c85cb72cffeb
David L. Inman <davidlevere@gmail.com>
2020-05-01 17:26:32 -0400

    TODO for top level

--- src/parsing/top_level.rs
+++ src/parsing/top_level.rs
@@ -4,6 +4,15 @@ use super::parse_error::ParseError;
 use super::input::Input;
 
 impl<'a> Input<'a> {
+    // TODO struct def
+    // TODO enum def
+    // TODO fun def
+    // TODO impl def
+        // TODO type def
+    // TODO mod
+    // TODO trait def
+        // TODO func sig
+        // TODO type sig (need way to indicate if it is an owned type (maybe do that in the function def?))
     pub fn parse_type_def(&self) -> Result<TypeDef, ParseError> {
         Err(ParseError::EndOfFile(format!("blarg")))
     }
