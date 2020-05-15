0d4c4600af507b56c95f042565880a2b0a9fbc25
David L. Inman <davidlevere@gmail.com>
2020-05-14 23:41:21 -0400

    more tests for enum parse

--- src/parsing/top_level.rs
+++ src/parsing/top_level.rs
@@ -519,4 +519,38 @@ enum some {
 
         Ok(())
     }
+
+    #[test]
+    fn should_parse_enum_field_def_with_struct_case() -> Result<(), ParseError> { 
+        let i = r#"
+enum some {  
+    Blah { a : T, b : T },
+    Blah { a : T, b : T },
+} "#.char_indices().collect::<Vec<(usize, char)>>();
+        let mut input = Input::new(&i);
+        let mut u = input.parse_enum_def()?;
+
+        assert_eq!( u.name, "some" );
+        assert_eq!( u.cases.len(), 2 );
+        assert_eq!( u.type_params.len(), 0 );
+
+        Ok(())
+    }
+
+    #[test]
+    fn should_parse_enum_field_def_with_type_case() -> Result<(), ParseError> { 
+        let i = r#"
+enum some {  
+    Blah (T1, T2, T3),
+    Blah (T1, T2),
+} "#.char_indices().collect::<Vec<(usize, char)>>();
+        let mut input = Input::new(&i);
+        let mut u = input.parse_enum_def()?;
+
+        assert_eq!( u.name, "some" );
+        assert_eq!( u.cases.len(), 2 );
+        assert_eq!( u.type_params.len(), 0 );
+
+        Ok(())
+    }
 }
