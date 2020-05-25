ca832eff6da2094462aa17dfd6595d5dae9be651
David L. Inman <davidlevere@gmail.com>
2020-05-25 11:08:15 -0400

    starting off expr

---
+++ src/parsing/expr.rs
@@ -0,0 +1,51 @@
+
+use super::ast::*;
+use super::parse_error::ParseError;
+use super::input::Input;
+
+impl<'a> Input<'a> {
+    // TODO : lambda
+    // TODO : number
+    // TODO : string
+    // TODO : list
+    // TODO : dictionary
+    // TODO : loop
+    // TODO : foreach
+    // TODO : match
+    // TODO : let
+    // TODO : mut
+    // TODO : if
+    // TODO : assert / panic
+    // TODO : try
+    // TODO : return
+    // TODO : dot 
+    // TODO : { }
+    // TODO : ( )
+    // TODO : yield return (?)
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+   /* #[test]
+    fn should_parse_type_param_list() -> Result<(), ParseError> {
+        let i = "<A : B, C : D + E + F, G, H>".char_indices().collect::<Vec<(usize, char)>>();
+        let mut input = Input::new(&i);
+        let u = input.parse_type_param_list()?;
+        assert_eq!( u.len(), 4 );
+
+        assert_eq!( u[0].name, "A" );
+        assert_eq!( u[0].constraints.len(), 1 );
+        assert_eq!( u[0].constraints[0], "B" );
+
+        assert_eq!( u[1].name, "C" );
+        assert_eq!( u[1].constraints.len(), 3 );
+        assert_eq!( u[1].constraints[0], "D" );
+        assert_eq!( u[1].constraints[1], "E" );
+        assert_eq!( u[1].constraints[2], "F" );
+        Ok(())
+    }*/
+
+}
--- src/parsing/mod.rs
+++ src/parsing/mod.rs
@@ -3,6 +3,7 @@ pub mod ast;
 mod parse_error;
 mod input;
 mod misc; 
+mod expr;
 mod top_level;
 mod parser; 
 
